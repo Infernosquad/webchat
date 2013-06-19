84bf3b5ba60102be476c71880edaa171262c649b
Artem Zhuravlev <infzaznoza@gmail.com>
2013-06-19 21:00:04 +0600

    Initital commit

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <script type="text/javascript" src="js/socket.io.min.js"></script>
+    <title></title>
+</head>
+<body>
+   <h1>Hello</h1> 
+</body>
+</html>
+
