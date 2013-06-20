74cf18ba85cdf1e13add9f69055939db853b8e94
Artem Zhuravlev <infzaznoza@gmail.com>
2013-06-20 21:38:07 +0600

    add github fork me badge

--- web/index.php
+++ web/index.php
@@ -14,6 +14,11 @@
     <script type="text/javascript" src="js/main.js"></script>
 </head>
 <body>
+    <a href="https://github.com/Infernosquad/webchat">
+        <img style="position: absolute; top: 0; right: 0; border: 0;" 
+        src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" 
+        alt="Fork me on GitHub">
+    </a>
     <div class="container">
 
         <div class="page-header">
